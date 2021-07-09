0fc905ffc75f2c0893c7be7104e76b84b895a8fd
basti982 <45665545+basti982@users.noreply.github.com>
2021-07-09 10:51:47 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,48 +1,46 @@
 #!/bin/sh
 #################### REQUIREMENTS #####################################
-# - Install p7zip (e.g. sudo apt-get install p7zip p7zip-full p7zip-rar)
+# - Install zip
 # - Change the path to the config.php file
 #######################################################################
 # Backup Data folder and SQL Dump
 copy () {
 	mkdir $PWD/backup;
-	cp -vR $dataroot $PWD/backup/moodledata.backup;
-	cp -vR $moodlepath $PWD/backup/moodle.backup;
-	mysqldump -h $dbhost -u $dbuser --password=$dbpass -C -Q -e --create-options $dbname > moodle-database.sql;
-	mv moodle-database.sql $PWD/backup/;
+	cp -vR $phplistpath $PWD/backup/phplist.backup;
+	mysqldump --no-tablespaces -h $dbhost -u $dbuser --password='jqx1M85_' -C -Q -e --create-options $dbname > phplist-database.sql;
+	mv phplist-database.sql $PWD/backup/;
 	}
 
 # Compress backup files	
 compress (){
-	7z a moodle-backup.7z $PWD/backup/;
+	zip -r phplist-backup.zip $PWD/backup/;
 	}
 
 # Remove working directory
 remove (){
 	rm -R $PWD/backup;
-	rm $PWD/moodle-backup.7z;
+	rm $PWD/phplist-backup.zip;
 	}
 
 # Rename 7 Zip-file to add date stamp	
 rename (){
-	file_name=moodle-backup.7z
+	file_name=phplist-backup.zip
  	current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 	echo "Current Time : $current_time"
 	new_fileName=$current_time.$file_name
 	echo "New FileName: " "$new_fileName"
- 	cp $file_name $new_fileName
+	cp $file_name $new_fileName
 	}
 
-# The path to the moodle config.php
-moodlepath=/var/www/html/moodle
-configpath=$moodlepath/config.php 
+# The path to the phpconfig config.php
+phplistpath=/httpdocs/newsletter/phplist
+configpath=/httpdocs/newsletter/phplist/public_html/lists/config/config.php 
 
 # Grep user, pw, host, dbname
-dbuser=$(grep dbuser $configpath | grep -Eo "'[A-Za-z0-9][A-Za-z0-9]*"| grep -Eo "[A-Za-z0-9][A-Za-z0-9]*")
-dbhost=$(grep dbhost $configpath | grep -Eo "'[A-Za-z0-9][A-Za-z0-9]*"| grep -Eo "[A-Za-z0-9][A-Za-z0-9]*")
-dbname=$(grep dbname $configpath | grep -Eo "'[A-Za-z0-9][A-Za-z0-9]*"| grep -Eo "[A-Za-z0-9][A-Za-z0-9]*")
-dbpass=$(grep dbpass $configpath | grep -Eo "'[A-Za-z0-9][A-Za-z0-9]*"| grep -Eo "[A-Za-z0-9][A-Za-z0-9]*")
-dataroot=$(grep dataroot $configpath | grep -Eo "[/][A-Za-z0-9/]*")
+dbuser=$(grep database_user $configpath | grep -Eo \'[A-Za-z0-9\_]+\' | grep -Eo [A-Za-z0-9\_]+)
+dbhost=$(grep database_host $configpath | grep -Eo \'[A-Za-z0-9\.]+\' | grep -Eo [A-Za-z0-9\.]+)
+dbname=$(grep database_name $configpath | grep -Eo \'[A-Za-z0-9\_]+\' | grep -Eo [A-Za-z0-9\_]+)
+dbpass=$(grep database_password $configpath | grep -Eo \'[A-Za-z0-9\_]+\' | grep -Eo [A-Za-z0-9\_]+)
 
 # Run the functions
 copy&&compress&&rename&&remove;
